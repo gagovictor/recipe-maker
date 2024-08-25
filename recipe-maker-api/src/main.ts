7ad64edfca585852f077daccd2727d3e99a58627
Victor Gago <victorgago-work@outlook.com>
2024-08-25 17:35:53 -0300

    CORS support for UI

--- recipe-maker-api/src/main.ts
+++ recipe-maker-api/src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (['http://localhost:4200'].indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'POST',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
